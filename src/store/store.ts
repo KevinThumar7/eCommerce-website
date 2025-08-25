5183a90d0d808992a4b76b3085c85b008f160753
KevinThumar7 <kevinthumar36@gmail.com>
2025-08-25 16:02:46 +0530

    store updated for storing state in localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,33 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from "../counter/counterSlice"
 import cartReducer from "../counter/cartSlice"
 
+const getState = () => {
+  try {
+    const loadedState = localStorage.getItem("cartState")
+    return loadedState ? JSON.parse(loadedState) : undefined
+  } catch {
+    return undefined
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const saveState = (state: any) => {
+  try { localStorage.setItem("cartState", JSON.stringify(state)) }
+  catch { console.error() }
+}
+
 export const store = configureStore({
-    reducer: {
-      counter: counterReducer,
-      cart: cartReducer,
+  reducer: {
+    counter: counterReducer,
+    cart: cartReducer,
   },
+  preloadedState: {
+    cart: getState()
+  }
+})
+
+store.subscribe(() => {
+  saveState(store.getState().cart)
 })
 
 export type RootState = ReturnType<typeof store.getState>
